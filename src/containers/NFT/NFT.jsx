31baa1aa822c7bc33bcfe3e678833ede470a39d6
Chloe Lee <chloelee00516@gmail.com>
2022-07-06 18:22:00 +0900

    added an image in NFT section
    added a flower line png above the title

--- src/containers/NFT/NFT.jsx
+++ src/containers/NFT/NFT.jsx
@@ -12,28 +12,33 @@ import img5 from "../../assets/NFT-images/1697.png";
 
 const NFT = () => {
   return (
-    <AnimationOnScroll animateIn="animate__animated animate__fadeIn">
+    <AnimationOnScroll animateIn="animate__animated animate__fadeIn" afterAnimatedOut={"animate__fadeOut"}>
     <Parallax speed={-25}>
     <section className="NFT-section section__padding">
-        <h1 className="section__title">KPOP101</h1>
-        <h1 className="section__subtitle gradient__text">gulz collection</h1>
-        <div className="NFT-images-container content__padding">
-            <div>
-                <img src={img1} className="img1 nft-img" alt="example test images nft"/>
-            </div>
-            <div>
-                <img src={img2} className="img2 nft-img" alt="example test images nft"/>
-            </div>
-            <div>
-                <img src={img3} className="img3 nft-img" alt="example test images nft"/>
-            </div>
-            <div>
-                <img src={img4} className="img4 nft-img" alt="example test images nft"/>
-            </div>
-            <div>
-                <img src={img5} className="img5 nft-img" alt="example test images nft"/>
+        <div className="girls-collection-container">
+            <div className="NFT-title-line"></div>
+            <h1 className="section__title">KPOP101</h1>
+
+            <h1 className="section__subtitle gradient__text">gulz collection</h1>
+            <div className="NFT-images-container content__padding">
+                <div>
+                    <img src={img1} className="img1 nft-img" alt="example test images nft"/>
+                </div>
+                <div>
+                    <img src={img2} className="img2 nft-img" alt="example test images nft"/>
+                </div>
+                <div>
+                    <img src={img3} className="img3 nft-img" alt="example test images nft"/>
+                </div>
+                <div>
+                    <img src={img4} className="img4 nft-img" alt="example test images nft"/>
+                </div>
+                <div>
+                    <img src={img5} className="img5 nft-img" alt="example test images nft"/>
+                </div>
             </div>
         </div>
+
         
     </section>
     </Parallax>
